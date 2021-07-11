9aa07179d463df0aa12b36817fcc5e7ca98da65b
Hikmet Dogrul <hikmetdogrul00@gmail.com>
2021-07-11 00:03:21 -0400

    adding time conversion

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,20 @@
-var url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var today = new Date();
+var month = today.getUTCMonth() + 1; //months from 1-12
+var day = today.getUTCDate();
+var year = today.getUTCFullYear();
+
+var url = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 var apiKey = '&appid=6381ffc04045f8a634c40c9d3d759174';
 var searchTxtBox = document.getElementById('search-input').value;
 var searchBtn = document.getElementById("search-button");
+var weatherData;
 
 
+console.log(url + searchTxtBox + apiKey)
 function getCityWeatherData() {
+    // searchTxtBox ="";
+    searchTxtBox= document.getElementById('search-input').value;
+
     fetch(
         url + searchTxtBox + apiKey
     )
@@ -16,12 +26,13 @@ function getCityWeatherData() {
                 console.log(error);
             }
         })
-        .then(function (data, error) {
-            if (data !== undefined) {
-                console.log(data.name);
-                console.log(data.main.temp);
-            } else {
-                console.log(error);
-            }
-        });
+        .then( (data)=> {
+            weatherData = data;
+            });
+}
+
+function toDateTime(secs) {
+    var t = new Date(1970, 0, 1);
+    t.setSeconds(secs);
+    return t.getDate() + '/' + (t.getMonth()+1) + '/' + t.getFullYear();
 }
